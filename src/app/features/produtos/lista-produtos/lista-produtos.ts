e2678d93938244641a3b93cc5bf16c152b743f51
Nayra Gama <nayrafgama263@gmail.com>
2026-07-17 21:31:26 -0300

    adiciona imports, substituição de lista, monitoramento em tempo real e estado de seleção

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -3,9 +3,12 @@ import { Produto } from '../produto/produto';
 import { signal } from '@angular/core';
 import { computed } from '@angular/core';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
+import { effect } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto,  PrecoFormatadoPipe],
+  imports: [Produto,  PrecoFormatadoPipe, UpperCasePipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
@@ -20,6 +23,7 @@ export class ListaProdutos {
   //!função para exibir produtos selecionados pelo usuarios no console
   exibirProduto (nome: string){
     console.log ('Produto Selecionado: ', nome);
+    this.produtoSelecionado.set(nome);
   }
   //!função que adicionar produtos usando metodo update
   adicionarProduto(){
@@ -33,7 +37,32 @@ export class ListaProdutos {
   //!função que calcula o valor total do produtos usando o metado computed
   valorTotal = computed(()=>
   {return this.produtos().reduce((total, item) =>
-    total + item.preco,0
-  )}
-);
+    total + item.preco,0)});
+    //!função que substituir a lista atual usando metodo set()
+    substituirProdutos(){
+      this.produtos.set([
+        {nome:'Teclado', preco: 50},
+        {nome:'Mouse', preco: 15},
+        {nome:'Monitor', preco: 500},
+        {nome:'Desktop', preco: 1500},
+        {nome:'headset', preco: 30},
+      ])
+    }
+    //! metodo para monitorar alterações em tempo real usando effect
+    constructor(){
+      effect(() => {
+        console.log ('Lista de Produtos Alterados: ', this.produtos());
+      });
+      effect(() => {
+        console.log('Valor Total Atualizado: ', this.valorTotal());
+      });
+      effect(() => {
+        if (typeof document !== 'undefined'){
+       document.title = `(${this.totalProdutos()}) -Loja da Titia Nayra`;
+        }
+      });
+    }
+    //! Metodo para criar um estado de seleção com signal string | null
+    produtoSelecionado = signal <string | null>(null);
+
 }
