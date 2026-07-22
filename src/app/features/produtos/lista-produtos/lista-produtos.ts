241e6039bfa29e657622c10ac62c00fcd5413c1d
Nayra Gama <nayrafgama263@gmail.com>
2026-07-22 19:31:38 -0300

    feat (carrinho): implementar gerenciamento de estado com signals

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -58,11 +58,25 @@ export class ListaProdutos {
       });
       effect(() => {
         if (typeof document !== 'undefined'){
-       document.title = `(${this.totalProdutos()}) -Loja da Titia Nayra`;
+       document.title = `(${this.totalProdutos()}) -Loja da Nayra`;
         }
       });
     }
     //! Metodo para criar um estado de seleção com signal string | null
     produtoSelecionado = signal <string | null>(null);
-
-}
+    //! metodo para criar um estado para carrinho com signal
+    carrinho = signal <{nome: string; preco: number}[]>([]);
+    adicionarAoCarrinho(produto:{nome: string; preco: number}){
+      this.carrinho.update(listaAtual =>[...listaAtual, produto]
+        );
+          }
+  //! totalProdutos = computed(() => this.produtos().length;
+  //metodo para calcular a quantidade total de itns no carrinho
+  quantidadeCarrinho = computed(() => this.carrinho().length);
+  //metodo para calcular o valor total dos itens do carrinho
+  totalCarrinho = computed(() => {
+    return this.carrinho().reduce((total, item) =>
+    total + item.preco,0)});
+    //! valorTotal = computed(() => {
+    //! return this.produtos().reduce((total, item) =>     
+ }
