e94ffb23e1bdeffd4878ced1a8ff40913a97f3fa
Nayra Gama <nayrafgama263@gmail.com>
2026-07-22 19:38:18 -0300

    feat: foi criado a estrutura inicial componente carrinho

---
+++ src/app/freatures/carrinho/carrinho/carrinho.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho',
+  imports: [],
+  templateUrl: './carrinho.html',
+  styleUrl: './carrinho.css',
+})
+export class Carrinho {
+   //! Lígica de carrinho virá na proximas aulas
+   //! Por ora, apenas exibe a página carrinho
+}
