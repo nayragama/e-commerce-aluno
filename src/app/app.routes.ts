225332cc23752ba3cc989efaed10ae4143fecb6c
Nayra Gama <nayrafgama263@gmail.com>
2026-07-22 21:42:23 -0300

    feat(app):integra funções de autenticação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,34 @@
 import { Routes } from '@angular/router';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
-import { Carrinho } from './freatures/carrinho/carrinho/carrinho';
+import { authGuard } from './core/auth.guard';
 
 export const routes: Routes = [
-    {
-        path:'',
-        component: ListaProdutos,
-    },
-    {
-        path:'carrinho',
-        component: Carrinho,
-    },
+   {
+    path:'', //router para reiz localhost:4200/
+    loadComponent: () =>
+        import('./features/home/home/home/home')
+    .then((m) => m.Home),
+   },
+   {
+    path:'produtos',
+    loadComponent: () =>
+        import('./features/produtos/lista-produtos/lista-produtos')
+    .then((m) => m.ListaProdutos),
+   },
+   {
+    path:'carrinho',
+    canActivate: [authGuard],
+    loadComponent: () =>
+        import('./features/carrinho/carrinho/carrinho')
+    .then((m) => m.Carrinho),
+   },
+   {
+    path:'home',
+    loadComponent: () =>
+        import('./features/home/home/home/home')
+    .then((m) => m.Home),
+   },
+   {
+    path:'**',
+    redirectTo: '',
+   },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { UpperCasePipe } from '@angular/common';
-
+import { usuarioLogado,login,logout } from './core/auth';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink, UpperCasePipe],
@@ -11,4 +11,7 @@ import { UpperCasePipe } from '@angular/common';
 export class App {
   protected readonly title = signal('e-commerce-aluno');
   nomeLoja = 'Loja Da Nayra';
+  usuarioLogado = usuarioLogado;
+  login = login;
+  logout = logout;
 }
