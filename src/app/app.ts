edc9c0fb33a154a8c13be613654e42ff4ac38859
Nayra Gama <nayrafgama263@gmail.com>
2026-08-05 22:07:25 -0300

    feat: integra cabeçalho e controles de autenticação

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { UpperCasePipe } from '@angular/common';
 import { usuarioLogado,login,logout } from './core/auth';
+import { MatButtonModule } from '@angular/material/button';
+import { Header } from './shared/layout/header/header';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, UpperCasePipe],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe,MatButtonModule,Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
