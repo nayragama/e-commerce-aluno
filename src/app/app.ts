2d6c19a739803376e54326c9f512a96f0619f5c2
Nayra Gama <nayrafgama263@gmail.com>
2026-08-14 20:56:36 -0300

    feat: atualiza componete principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { UpperCasePipe } from '@angular/common';
-import { usuarioLogado,login,logout } from './core/auth';
 import { MatButtonModule } from '@angular/material/button';
 import { Header } from './shared/layout/header/header';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, UpperCasePipe,MatButtonModule,Header],
+  imports: [RouterOutlet, MatButtonModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce-aluno');
   nomeLoja = 'Loja Da Nayra';
-  usuarioLogado = usuarioLogado;
-  login = login;
-  logout = logout;
 }
