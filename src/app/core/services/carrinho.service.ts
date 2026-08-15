43405fc5e8bc168e80099e43c7455f109e34231b
Nayra Gama <nayrafgama263@gmail.com>
2026-08-14 21:01:35 -0300

    feat: atualiza serviço do carrinho

--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -2,23 +2,25 @@ import { Injectable } from "@angular/core";
 import { signal } from "@angular/core";
 import { computed } from "@angular/core"
 
-@Injectable({
-    providedIn: 'root'
-})
+type ItemCarrinho = {
+    nome: string;
+    preco: number;
+}
 
-export class CarrinhoServise {
+@Injectable({providedIn: 'root'})
 
+export class CarrinhoService {
 //! Estado Global - Criado com Sucesso
-private carrinho = signal<{nome: string; preco: number}[]>([])
+private carrinho = signal<ItemCarrinho[]>([]);
 
 //? Seleção
 itens = computed (() => this.carrinho());
 quantidadeItens = computed(()=> this.carrinho().length);
-totalItens = computed(() =>
-this.carrinho().reduce((total, item) => total + item.preco,0));
+totalItens = computed(() => this.carrinho().reduce((total, item) => total + item.preco,0));
+carrinhoVazio = computed (() => this.carrinho().length ===0);
 
 // TODO: Ação Adicionar Produtos
-adicionar(produto:{nome:string; preco:number}){
+adicionar(produto:ItemCarrinho){
     this.carrinho.update(lista => [...lista, produto]);
 }
 // TODO: Ação de Limpeza
