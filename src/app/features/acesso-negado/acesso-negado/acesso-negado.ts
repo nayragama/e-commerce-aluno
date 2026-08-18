67f95d0bf8a89ce074dbb7a1cb8579bc6e04fffe
Nayra Gama <nayrafgama263@gamil.com>
2026-08-18 20:52:49 -0300

    feat:adiciona tela de acesso negado

---
+++ src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+selector: 'app-acesso-negado',
+imports: [RouterLink, MatButtonModule],
+templateUrl: './acesso-negado.html',
+styleUrl: './acesso-negado.css',
+})
+export class AcessoNegado {
+
+  private router = inject(Router);
+  private authService = inject(AuthService);
+
+  Sair(){
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+    return;
+  }
+}
