6df2d49566ae97dcc8e166fa7ea80b451cb0111f
Nayra Gama <nayrafgama263@gamil.com>
2026-08-18 21:37:44 -0300

    feat: atualiza estrurura de area administrativa

--- src/app/features/admin/admin/admin.ts
+++ src/app/features/admin/admin/admin.ts
@@ -1,6 +1,5 @@
 import { Component, computed, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
-
 import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
