6f2a0faa763a107020e2cc7c3b2a6eb4c248cd84
Nayra Gama <nayrafgama263@gamil.com>
2026-08-19 19:35:29 -0300

    refactor: organiza guards em pasta dedicada

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivateFn} from "@angular/router";
+import { AuthService } from "./services/auth.service";
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+
+export const authGuard: CanActivateFn = () => {
+    const authService = inject(AuthService);
+    const router = inject(Router);
+
+    if(authService.usuarioLogado()){
+        return true;
+    }
+    return router.createUrlTree(['/login']);
+}    
