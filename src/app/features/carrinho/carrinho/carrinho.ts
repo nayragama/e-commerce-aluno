363584c31bbff853a8330b72604625b116cb7702
Nayra Gama <nayrafgama263@gamil.com>
2026-08-20 20:23:27 -0300

    feat: atualiza interface e funcionalidades do carrinho

--- src/app/features/carrinho/carrinho/carrinho.ts
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink,Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { CarrinhoFacade } from '../../../core/facades/carrinho.facade';
+import { AuthFacade } from '../../../core/facades/auth.facade';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 
 @Component({
   selector: 'app-carrinho',
-  imports: [],
+  imports: [MatButtonModule, RouterLink, PrecoFormatadoPipe],
   templateUrl: './carrinho.html',
   styleUrl: './carrinho.css',
 })
 export class Carrinho {
-   //! Lígica de carrinho virá na proximas aulas
-   //! Por ora, apenas exibe a página carrinho
+   private router = inject(Router);
+   public carrinhoFacade = inject(CarrinhoFacade);
+   private authFacade = inject(AuthFacade);
+
+   removerItem(rmvItem:number){
+    this.carrinhoFacade.removerItem(rmvItem);
+   }
+
+   limparCarrinho(){
+    this.carrinhoFacade.limparCarrinho();
+   }
+
+   cancelarCompra(){
+    this.carrinhoFacade.limparCarrinho();
+    this.authFacade.sair();
+    this.router.navigateByUrl('/login');
+   }
+
 }
