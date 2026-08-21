63356d618a5cc66f5721711f3b84b27f0af46030
Nayra Gama <nayrafgama263@gmail.com>
2026-08-21 20:05:23 -0300

    feat:atualiza estrutura do carrinho

--- src/app/core/facades/carrinho.facade.ts
+++ src/app/core/facades/carrinho.facade.ts
@@ -2,25 +2,27 @@ import { Injectable, inject } from '@angular/core';
 import { CarrinhoService } from '../services/carrinho.service';
 import { ItemCarrinho } from '../models/item-carrinho';
 
-@Injectable({providedIn: 'root',})
-
+@Injectable({
+   providedIn: 'root',
+})
 export class CarrinhoFacade {
+   
     private carrinhoService = inject(CarrinhoService);
 
     itensCarrinho = this.carrinhoService.itens;
-    quantidadeCarrinho = this.carrinhoService.quantidadeItens;
-    totalCarrinho = this.carrinhoService.totalItens;
+    quantidadeCarrinho = this.carrinhoService.quantidade;
+    totalCarrinho = this.carrinhoService.total;
     carrinhoVazio = this.carrinhoService.carrinhoVazio;
+   
+    adicionarProduto(produto: ItemCarrinho) {
+        this.carrinhoService.adicionar(produto);
+    }
 
-    adicionarProdutoCarrinho(produto: ItemCarrinho) {
-       this.carrinhoService.adicionar(produto);
+    removerItem(index: number) {
+        this.carrinhoService.removerPorIndice(index);
     }
 
     limparCarrinho() {
         this.carrinhoService.limpar();
-    }
-
-    removerItem(rmvItem:number){
-        this.carrinhoService.removerItem(rmvItem);
     }
 }
--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -1,31 +1,28 @@
-import { Injectable } from "@angular/core";
-import { signal } from "@angular/core";
-import { computed } from "@angular/core"
-import { ItemCarrinho } from "../models/item-carrinho"
+import { Injectable, computed, signal } from '@angular/core';
+import { ItemCarrinho } from '../models/item-carrinho';
 
-@Injectable({providedIn: 'root'})
-
+@Injectable({
+    providedIn: 'root',
+})
 export class CarrinhoService {
-//! Estado Global - Criado com Sucesso
-private carrinho = signal<ItemCarrinho[]>([]);
 
-//? Seleção
-itens = computed (() => this.carrinho());
-quantidadeItens = computed(()=> this.carrinho().length);
-totalItens = computed(() => this.carrinho().reduce((total, item) => total + item.preco,0));
-carrinhoVazio = computed (() => this.carrinho().length ===0);
+    private carrinho = signal<ItemCarrinho[]>([]);
+    itens = computed(() => this.carrinho());
+    quantidade = computed(() => this.carrinho().length);
+    total = computed(() =>
+        this.carrinho().reduce((total, item) => total + item.preco, 0)
+    );
 
-// TODO: Ação Adicionar Produtos
-adicionar(produto:ItemCarrinho){
-    this.carrinho.update(lista => [...lista, produto]);
-}
-// TODO: Ação de Limpeza
-limpar(){
-    this.carrinho.set([]);
-}
-
-removerItem(rmvItem:number){
-    this.carrinho.update((listaAtual) =>
-    listaAtual.filter((_, index) => index !== rmvItem));
-}
+    carrinhoVazio = computed(() => this.carrinho().length === 0);
+    adicionar(produto: ItemCarrinho) {
+        this.carrinho.update((listaAtual) => [...listaAtual, produto]);
+    }
+    removerPorIndice(indice: number) {
+        this.carrinho.update((listaAtual) =>
+            listaAtual.filter((_, index) => index !== indice)
+        );
+    }
+    limpar() {
+        this.carrinho.set([]);
+    }
 }
